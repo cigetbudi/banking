1b27cbb59854ff6d87bcf89e808dffd137ac93bd
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-06-04 18:02:50 +0700

    Routing Basics - Gorilla Mux

--- app/app.go
+++ app/app.go
@@ -1,16 +1,31 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 	//ROUTE
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/getAll", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	router.HandleFunc("/customers/{customer_Id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	//SERVER
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_Id"])
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post diterima")
 }
